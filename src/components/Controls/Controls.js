005e870b9b9b8ba9ebe9112cd14002fc139a1b12
Leonid Kuznetsov <leonid.shmidt@gmail.com>
2018-07-06 17:43:30 +0600

    Implemented loading of state and copying of state into clipboard

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 
+import copyToClipboard from 'copy-to-clipboard';
+
 import store from '../../store';
 
 let nextGroupId = 1;
@@ -9,6 +11,7 @@ Vue.component('Controls', {
   data: () => ({
     newGroupTitle: '',
     newItemTitle: '',
+    serializedState: '',
   }),
 
   computed: {
@@ -19,14 +22,15 @@ Vue.component('Controls', {
 
   methods: {
     createGroup () {
-      if (!this.$data.newGroupTitle) {
+      const { newGroupTitle } = this.$data;
+      if (!newGroupTitle) {
         return;
       }
 
       store.dispatch('entities/groups/insert', {
         data: {
           id: nextGroupId++,
-          title: this.$data.newGroupTitle,
+          title: newGroupTitle,
           items: null,
         },
       });
@@ -35,33 +39,110 @@ Vue.component('Controls', {
     },
 
     createItem () {
-      if (!this.$data.newItemTitle) {
+      const { newItemTitle } = this.$data;
+      if (!newItemTitle) {
         return;
       }
 
-      const selectedGroupId = store.getters.selectedGroupId;
-
-      if (selectedGroupId) {
-        store.dispatch('entities/items/insert', {
-          data: {
-            id: nextItemId++,
-            groupId: selectedGroupId,
-            title: this.$data.newItemTitle,
-            checked: false,
-          },
-        });
+      const { selectedGroupId } = store.getters;
+      if (!selectedGroupId) {
+        return;
       }
+
+      store.dispatch('entities/items/insert', {
+        data: {
+          id: nextItemId++,
+          groupId: selectedGroupId,
+          title: newItemTitle,
+          checked: false,
+        },
+      });
+
+      this.$data.newGroupTitle = '';
+    },
+
+    setState () {
+      const { serializedState } = this.$data;
+      if (!serializedState) {
+        return;
+      }
+
+      let newState;
+      try {
+        newState = JSON.parse(serializedState);
+      }
+      catch (error) {
+        // @todo Display nice message.
+        console.error(error);
+      }
+
+      if (!newState) {
+        return;
+      }
+
+      const getMaxId = (value, item) => Math.max(value, item.id);
+
+      const groups = [ ...newState.groups ];
+      nextGroupId = groups.reduce(getMaxId, 0) + 1;
+      store.dispatch('entities/groups/create', { data: groups });
+
+      const items = [ ...newState.items ];
+      nextItemId = items.reduce(getMaxId, 0) + 1;
+      store.dispatch('entities/items/create', { data: items });
+
+      store.commit('setSelectedGroupId', { groupId: newState.selectedGroupId || null });
+    },
+
+    copyState () {
+      const state = {
+        selectedGroupId: store.getters.selectedGroupId,
+        groups: store.getters['entities/groups/all'](),
+        items: store.getters['entities/items/all'](),
+      };
+      console.warn();
+      copyToClipboard(JSON.stringify(state));
+    },
+
+    loadState () {
+      // To be implemented.
     },
   },
 
+  // @todo There should be a way to move template into .vue file and using
+  // Vue.component at the same time.
   template: `
     <div>
       <div>
-        <input type="text" name="new-group-title" id="new-group-title" v-model.trim="newGroupTitle" />
+        <textarea
+          name="serialized-state"
+          id="serialized-state"
+          v-model="serializedState"
+          placeholder="Please paste state here in JSON format"
+        >
+        </textarea>
+
+        <input type="button" value="Set state" v-on:click="setState" />
+        <input type="button" value="Copy state to clipboard" v-on:click="copyState" />
+        <input type="button" value="Load state" v-on:click="loadState" />
+      </div>
+      <div>
+        <input
+          type="text"
+          name="new-group-title"
+          id="new-group-title"
+          v-model.trim="newGroupTitle"
+          placeholder="Group title"
+        />
         <input type="button" value="Create group" v-on:click="createGroup" />
       </div>
       <div v-if="hasSelectedGroup">
-        <input type="text" name="new-item-title" id="new-item-title" v-model.trim="newItemTitle" />
+        <input
+          type="text"
+          name="new-item-title"
+          id="new-item-title"
+          v-model.trim="newItemTitle"
+          placeholder="Item title"
+        />
         <input type="button" value="Create item" v-on:click="createItem" />
       </div>
     </div>
--- src/store/Item/Item.js
+++ src/store/Item/Item.js
@@ -1,14 +1,14 @@
 import { Model } from 'vuex-orm';
 
-import Group from '../Group/Group';
-
 export default class Item extends Model {
   static entity = 'items';
 
   static fields () {
     return {
       id: this.attr(null),
-      groupId: this.belongsTo(Group),
+      // @todo Using belongsTo causes groupId to be set to null when getting
+      // item object from state. I'll use default attr for now.
+      groupId: this.attr(null),
       title: this.attr(''),
       checked: this.attr(false),
     };
